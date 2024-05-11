41367ade541f118992ed04c1ee5621778fd0efee
elieljohn <ejmalabar88@gmail.com>
2024-05-11 10:44:26 +0100

    1. Add anchor tag and routerLink to convert productName to a clickable link product-list.component.html - line 46-48
    2. Retrieve the 'id' parameter from the current URL and use it to update the pageTitle.
    product-detial.component.ts - line 11-16

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }  // Provide access to the 'id' parameter passed in the URL
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));  // Retrieve 'id' parameter from the URL and store it to the 'id' constant
+    this.pageTitle += `: ${id}`;  // Append 'id' to the 'pageTitle' string
   }
 
 }
