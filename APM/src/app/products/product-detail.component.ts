d045e4bbb86bb42a82e5971a387aac901c155faa
elieljohn <ejmalabar88@gmail.com>
2024-05-11 11:01:05 +0100

    1. Add 'product' property with type IProduct | undefined product-detail.components.ts - line 11
    2. Append productName to pageTitle if product is not null
    product-detail.component.html - line 3
    
    3. Hard code product details on ngOnInit() to test
    product-details.components.ts - line 18-28

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,12 +8,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;  // 'product' will be undefined until the data is retrieved from the HTTP
 
   constructor(private route: ActivatedRoute) { }  // Provide access to the 'id' parameter passed in the URL
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));  // Retrieve 'id' parameter from the URL and store it to the 'id' constant
     this.pageTitle += `: ${id}`;  // Append 'id' to the 'pageTitle' string
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2021",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    };
   }
-
 }
